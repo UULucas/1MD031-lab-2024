71e2ad07f09d78daee0f8b10c793d1e499fa28ec
UURemy <lucas.enroth.1275@student.uu.se>
2024-12-03 19:11:19 +0100

    klar med 75% av guiden, ska precis börja med dispatch delen

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    {{ burger.name }} {{ burger.kCal }}
+  <div class="spaced">
+    <section>
+      <h1>{{ burger.name }}</h1>
+    </section>
+    <div>{{ amountOrdered }}</div>
+    <img v-bind:src="burger.url" alt="span"
+         title="OG Burger" style="width: 200px">
+    <ul>
+      <li><section class="classname">Contains <span id="idname">lactose</span></section></li>
+      <li>Contains red onions</li>
+      <li>Contains lactose</li>
+    </ul>
+    <button @click="increaseOrder">+</button>
+    <button @click="decreaseOrder">-</button>
   </div>
 </template>
 
@@ -9,11 +21,55 @@ export default {
   name: 'OneBurger',
   props: {
     burger: Object
-  }
+  },
+  data: function() {
+    return {
+      amountOrdered: 0,
+    }
+  },
+  methods: {
+    increaseOrder() {
+      this.amountOrdered++;
+      console.log("Updated amount: " + this.amountOrdered);
+      this.$emit('orderedBurger', {
+        name: this.burger.name,
+        amount: this.amountOrdered
+      });
+    },
+    decreaseOrder() {
+      if (this.amountOrdered > 0) {
+        this.amountOrdered--;
+        console.log("Updated amount: " + this.amountOrdered);
+        this.$emit('orderedBurger', {
+          name: this.burger.name,
+          amount: this.amountOrdered
+        });
+      }
+    }
+  },
+  addBurger: function () {
+    this.amountOrdered += 1;
+    this.$emit('orderedBurger', { name:   this.burger.name,
+          amount: this.amountOrdered
+        }
+    );
+  },
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.spaced {
+  margin: 10px;
+}
 
+.classname {
+  color: #000000;
+}
+
+#idname {
+  text-transform: uppercase;
+  font-weight: bold;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,70 @@
 <template>
-<div>
-    <div>
-    <h1>Burgers</h1>
-    <Burger v-for="burger in burgers"
-            v-bind:burger="burger" 
-            v-bind:key="burger.name"/>
-    </div>
-    <div id="map" v-on:click="addOrder">
-    click here
-    </div>
-  <input type="text" v-bind:value="yourVariable" v-on:input="yourVariable = $event.target.value">
-  <div>
-    {{ yourVariable }}
-  </div>
-</div>
+  <header id="header">
+    <div class="textImg">Burgers Online</div>
+    <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fast-food-design-template-486715096606ce499231c2736a6effba_screen.jpg?ts=1644097555" id="headerImg">
+  </header>
+  <main>
+    <section id="Burgers">
+      <Burger v-for="burger in burgers"
+              v-bind:burger="burger"
+              v-bind:key="burger.name"
+              v-on:orderedBurger="addToOrder($event)"/>
+    </section>
+    <section class="customers">
+      <h3>Customer Information</h3>
+      <p>This is where you provide necessary information</p>
+      <h4>Delivery Information:</h4>
+      <form>
+        <p>
+          <label for="full name">Full name</label><br>
+          <input type="text" id="full name" v-model="fullName" required="required" placeholder="First- and Last name">
+        </p>
+        <p>
+          <label for="email">Email</label><br>
+          <input type="email" id="email" v-model="email" required="required" placeholder="Email">
+        </p>
+        <p>
+          <label for="street">Street</label><br>
+          <input type="text" id="street" v-model="street" required="required" placeholder="Street">
+        </p>
+        <p>
+          <label for="house">House</label><br>
+          <input type="number" id="house" v-model="house" required="required" placeholder="House">
+        </p>
+        <p>
+          <label for="payment">Payment method</label><br>
+          <select id="payment method" v-model="paymentMethod">
+            <option>Cash</option>
+            <option selected="selected">Credit card</option>
+            <option>Gift card</option>
+            <option>Crypto</option>
+          </select>
+        </p>
+        <p>Please select your gender</p>
+        <input type="radio" id="male" value="Male" v-model="gender" required="required">
+        <label for="male">Male</label><br>
+        <input type="radio" id="female" value="Female" v-model="gender" required="required">
+        <label for="female">Female</label><br>
+        <input type="radio" id="dnwts" value="Do not wish to specify" v-model="gender" required="required">
+        <label for="male">Do not wish to specify</label><br>
+      </form>
+      <button type="button" id="orderButton" v-on:click="submitOrder">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPucVEkm69jXUSPgj6AmN4O13flENs9v-L6Q&s" style="width: 20px">
+        Order
+      </button>
+    </section>
+  </main>
+
+  <footer>
+    <hr>
+    UU Burgers &copy;
+  </footer>
 </template>
 
 <script>
 import Burger from '../components/OneBurger.vue'
 import io from 'socket.io-client'
+import menu from '../assets/menu.json'
 
 const socket = io("localhost:3000");
 
@@ -31,9 +77,11 @@ function MenuItem(name, kCal, url, gluten, lactose) {
 }
 
 const burgersArray = [
-  new MenuItem("Small Burger", 300, "small-burger.jpg", false, true),
-  new MenuItem("Standard Burger", 450, "standard-burger.jpg", true, true),
-  new MenuItem("Large Burger", 850, "large-burger.jpg", true, false)
+    // ...menu kmr göra att varje burgare i jsonen blir ett eget objekt i burgersArray
+    ...menu
+  //new MenuItem("KFC Burger", 300, "https://static.vecteezy.com/system/resources/thumbnails/022/911/694/small_2x/cute-cartoon-burger-icon-free-png.png", false, true),
+  //new MenuItem("OG Burger", 450, "https://www.burgerandsauce.com/wp-content/uploads/2021/02/burger-and-suace-chicken-burger-3000px-2.1.png", true, true),
+  //new MenuItem("Smashed Burger", 850, "https://lh5.googleusercontent.com/proxy/D8fDyPoDbzDV9mgGpTknMCObH456uxrslMpDm_WQlc3yKUxxcpvtGQMjdILDKpmtWcMFgedwfudpNbugw5bmBT1CeyADbGgkDbMVtSb16K4D", true, false)
 ];
 
 console.log(burgersArray);
@@ -48,6 +96,13 @@ export default {
     return {
       burgers: burgersArray,
       yourVariable: 'Välj en burgare',
+      fullName: '',
+      email: '',
+      street: '',
+      house: '',
+      paymentMethod: '',
+      gender: '',
+      orderedBurgers: {},
     }
   },
   methods: {
@@ -63,15 +118,151 @@ export default {
                                 orderItems: ["Beans", "Curry"]
                               }
                  );
+    },
+    addToOrder: function (event) {
+      if (event.amount > 0) {
+        this.orderedBurgers[event.name] = event.amount;
+      }
+      else {
+        delete this.orderedBurgers[event.name];
+      }
+      console.log(this.orderedBurgers); // För debugging
+    },
+    submitOrder: function () {
+      console.log("Order Details:");
+      console.log("Name:", this.fullName);
+      console.log("Email:", this.email);
+      console.log("Street:", this.street);
+      console.log("House:", this.house);
+      console.log("Payment Method:", this.paymentMethod);
+      console.log("Gender:", this.gender);
     }
-  }
+  },
 }
 </script>
 
 <style>
-  #map {
-    width: 300px;
-    height: 300px;
-    background-color: red;
-  }
+@import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Cormorant:wght@700&display=swap');
+@import 'https://fonts.googleapis.com/css?family=Pacifico|Dosis';
+body {
+  font-size: 12pt;
+}
+
+p {
+  /* color: white; */
+}
+
+h1 {
+  font-family: 'Inter Semi Bold', sans-serif;
+  font-size: 36pt;
+}
+main, header, footer, nav ul {
+  max-width: 40rem;
+  margin: 0 auto 0 auto;
+}
+main {
+  background-color: bisque;
+}
+
+/* nav ul li {
+    display: inline-block;
+    background-color: grey;
+    padding: 1em;
+    margin: 1em;
+} */
+
+
+#header {
+  position: relative;
+  width: 100%;
+  max-width: 600px;
+  margin: auto;
+  overflow: hidden;
+}
+
+#headerImg {
+  width: 100%;
+  height: auto;
+  display: block;
+  opacity: 0.5;
+  padding: 1em;
+}
+
+header h1 {
+  width:40rem;
+  margin: 0 auto;
+  text-align: center;
+}
+
+nav ul {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 9.25em);
+  gap: 1em;
+  padding: 0;
+}
+
+nav li {
+  display: block;
+  background-color: grey;
+  padding: 1em;
+}
+
+.Very-good {
+  color: green;
+}
+
+.Master {
+  color: green;
+  font-weight: bold;
+}
+
+.classname {
+  color: #000000;
+}
+
+#idname {
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+
+.customers {
+  color: #ffffff;
+  background-color: black;
+  margin: 20px;
+}
+
+button:hover {
+  background-color: blanchedalmond;
+  color: black;
+  cursor: pointer;
+}
+
+#Burgers {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 11em);
+  margin: 20px;
+  border: 10px solid #B99976;
+  padding: 10px;
+}
+
+#order\ button {
+  margin: 5px;
+}
+
+.spaced {
+  margin: 10px;
+}
+
+.textImg {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: black;
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+  pointer-events: none;
+}
 </style>
